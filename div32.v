d6a268966168e82a35660919e379d4907b018c32
mufeng-ck <chenkai_bupt@163.com>
2014-12-03 19:04:59 +0800

    fufill the div32

--- div32.v
+++ div32.v
@@ -4,28 +4,52 @@ module div32(
 	rst_n,
 	dataa,
 	datab,
-	result);
+	quotient);
 input clk;
-input rst;
+input rst_n;
 input[31:0] dataa;
 input[31:0] datab;
-output[31:0] result;
+output[31:0] quotient;//商
 
 reg[31:0] tempa[31:0], tempb[31:0];
-reg[31:0] result;
+reg[31:0] store[31:0];
+reg[31:0] result[31:0];
+always@(posedge clk or negedge rst_n)begin
+	if(~rst_n) begin
+		tempa[0] <= 0;
+		tempb[0] <= 0;
+		store[0] <= 0;
+		result[0] <= 0;
+	end
+	else begin
+		tempa[0] = {31'd0, dataa[31]};
+		tempb[0] = datab;
+		store[0] = dataa;
+		result[0] <= 0;
+	end
+end
 generate
 	genvar i;
-	for(i = 1; i< 31; i++)begin
-		always @(posedge clk or negedge rst_n) begin : proc_
+	for(i = 0; i< 31; i=i+1)begin
+		always @(posedge clk or negedge rst_n) begin:gen_
 			if(~rst_n) begin
-				tempa[i] <= 0;
-				tempb[i] <= 0;
-				result[i] <= 0;
+				tempa[i+1] <= 0;
+				tempb[i+1] <= 0;
+				result[i+1] <= 0;
 			end else begin
-				if(tempa[i] > tempb[i])begin
-					tempa[i + 1] <= (tempa[i] - tempb[i])<<1 + dataa[]
-				 <= ;
+				store[i+1] <= (store[i]<<1);
+				tempb[i + 1] <= tempb[i];
+				if(tempa[i] >= tempb[i])begin
+					tempa[i + 1] <= (tempa[i] - tempb[i])<<1 + store[i][31];
+					result[i + 1] <= (result[i]<<1) + 1;
+				end
+				else begin
+					tempa[i+1] <= (tempa[i] << 1) + store[i][31];
+					result[i+1] <= (result[i] << 1);
+				end
 			end
 		end
-
+	end
+endgenerate
+assign quotient = result[31];
 endmodule

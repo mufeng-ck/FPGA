e0fdc4a88b0337b6d82275e41aa602c69953ba52
mufeng-ck <chenkai_bupt@163.com>
2014-12-03 20:24:32 +0800

    the final of unsigned

--- div32.v
+++ div32.v
@@ -9,7 +9,7 @@ input clk;
 input rst_n;
 input[31:0] dataa;
 input[31:0] datab;
-output[31:0] quotient;//商
+output reg[31:0] quotient;//商
 
 reg[31:0] tempa[31:0], tempb[31:0];
 reg[31:0] store[31:0];
@@ -22,15 +22,21 @@ always@(posedge clk or negedge rst_n)begin
 		result[0] <= 0;
 	end
 	else begin
-		tempa[0] = {31'd0, dataa[31]};
-		tempb[0] = datab;
-		store[0] = dataa;
-		result[0] <= 0;
+		if(dataa[31] >= datab)begin
+			tempa[0] <= ({31'd0, dataa[31]} - datab)<<1 + dataa[30];
+			result[0] <= 1;
+		end
+		else begin 
+			tempa[0] <= {30'd0,dataa[31:30]};
+			result[0] <= 0;
+		end
+		tempb[0] <= datab;
+		store[0] <= {dataa[29:0], 2'd0};
 	end
 end
 generate
 	genvar i;
-	for(i = 0; i< 31; i=i+1)begin
+	for(i = 0; i< 30; i=i+1)begin
 		always @(posedge clk or negedge rst_n) begin:gen_
 			if(~rst_n) begin
 				tempa[i+1] <= 0;
@@ -40,7 +46,7 @@ generate
 				store[i+1] <= (store[i]<<1);
 				tempb[i + 1] <= tempb[i];
 				if(tempa[i] >= tempb[i])begin
-					tempa[i + 1] <= (tempa[i] - tempb[i])<<1 + store[i][31];
+					tempa[i + 1] <= ((tempa[i] - tempb[i])<<1) + store[i][31];
 					result[i + 1] <= (result[i]<<1) + 1;
 				end
 				else begin
@@ -51,5 +57,12 @@ generate
 		end
 	end
 endgenerate
-assign quotient = result[31];
+always@(posedge clk or negedge rst_n) begin
+	if(~rst_n)
+		quotient <= 0;
+	else begin 
+		if(tempa[30] >= tempb[30]) quotient <= (result[30] << 1) + 1;
+		else quotient <= (result[30]<<1);
+	end
+end
 endmodule
